1fff77d90a211025b387756e05c90645020da0ae
maerifat <76250027+maerifat@users.noreply.github.com>
2022-01-26 13:02:52 +0530

    Create wgen.py

---
+++ wgen.py
@@ -0,0 +1,61 @@
+import os
+import requests
+import re
+
+domainfile = open('dfile.txt', 'r')
+domains = domainfile.readlines()
+
+
+for domain in domains:
+
+    print(domain)
+    
+    cmd= (f"gospider -s {domain}  | egrep -o '{domain}.*$'| egrep -v '(\.png|\.css|\.svg|\.js|\.gif|\.jpg|\?)'|sort -u |tee -a file.txt; echo {domain} >>file.txt").replace('\r', '').replace('\n', '')
+
+    os.system(cmd)
+   #int(cmd)
+
+
+cmd2="sort -u -o file.txt file.txt"   
+os.system(cmd2)
+urlfile= open('file.txt', 'r')
+urls = urlfile.readlines()
+
+#for url in urls:
+#    response = requests.get(url)
+#    cont = response.content()
+#    print(cont)
+
+    
+wordlist  = []
+import requests
+from requests.exceptions import HTTPError
+
+for nonurl in urls:
+    url= nonurl.strip()
+    try:
+        response = requests.get(url)
+
+        # If the response was successful, no Exception will be raised
+        response.raise_for_status()
+    except HTTPError as http_err:
+        print(f'HTTP error occurred: {http_err}')  # Python 3.6
+    except Exception as err:
+        print(f'Other error occurred: {err}')  # Python 3.6
+    else:
+        
+        
+        for string in re.split('[\`\~\$&\+\*\!@#%_></\-,\":\';=\.\}\]\[\{\?\|\)\(\n\r\\\\ ]+', str(response.content)):
+            
+            if len(string) < 15 and len(string)> 1 and string.lower() not in wordlist and str(string.isdecimal())=="False":
+                
+                wordlist.append(string.lower())
+
+wordlist.sort()               
+for word in wordlist:
+    wordlistfile= open('wordlist.txt', 'a')
+    wordlistfile.write(f'{word}\n')
+    wordlistfile.close()
+print (len(wordlist))
+
+
